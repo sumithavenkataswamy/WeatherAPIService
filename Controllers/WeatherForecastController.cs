00cdaf80c162c97082036a605dd435e88b8f6c5f
Sumitha V <HappyGowda23@gmail.com>
2025-04-10 00:25:05 +0530

    Added weather service

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPIService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("custom-messages", Name = "GetCustomMessages")]
+        public IEnumerable<string> GetCustomMessages()
+        {
+            // Return a list of custom messages
+            return new[]
+            {
+                "Hello, welcome to the API!",
+                "Here’s your custom message.",
+                "Stay awesome!",
+                "Keep up the great work!",
+                "API responses made simple."
+            };
+        }
+    }
+}
